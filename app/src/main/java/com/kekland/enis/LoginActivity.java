1c354411c92fa6f581bbc1d6df29dde99cf7a74e
kekland <kk.erzhan@gmail.com>
2017-10-01 12:03:09 +0600

    First commit - eNIS 1.1

---
+++ app/src/main/java/com/kekland/enis/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.kekland.enis;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Gulnar on 01.10.2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
